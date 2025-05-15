a5df85b3b0dce2a1e2592e2eba9bb2f58b3338e9
KOOLUNKLE\a4336 <a43361904@gmail.com>
2025-05-15 18:36:03 +0900

    Setting up a second screen for navigation

--- app/src/main/java/com/example/navigation/FirstScreen.kt
+++ app/src/main/java/com/example/navigation/FirstScreen.kt
@@ -1,9 +1,5 @@
 package com.example.navigation
 
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -12,7 +8,6 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
@@ -24,16 +19,35 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.navigation.ui.theme.NavigationTheme
 
-class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            NavigationTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    FirstScreen(modifier = Modifier.padding(innerPadding))
-                }
-            }
+@Composable
+fun FirstScreen(modifier: Modifier = Modifier) {
+    val name = remember { mutableStateOf("") }
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Text(
+            text = "This is the First Screen",
+            fontSize = 24.sp,
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        OutlinedTextField(
+            value = name.value,
+            onValueChange = { name.value = it }
+        )
+        Button(onClick = {}) {
+            Text(text = "Go to Second Screen")
         }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun FirstPreview() {
+    NavigationTheme {
+        FirstScreen()
     }
 }
